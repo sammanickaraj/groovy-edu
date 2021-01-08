d1a17871d7eb362e2e3c71c974495b1dc9917c50
Sam Manickaraj <sammanickaraj@hotmail.com>
2021-01-09 01:43:36 +0530

    closure

--- Demo.groovy
+++ Demo.groovy
@@ -1,5 +1,7 @@
 public class Demo {
-    public static void main(String args[]) {
-        System.out.println("Hello World");
+    public static void main(String[] args) {
+        def str1 = "Hello"
+        def clos = { param -> println "${str1} ${param}" }
+        clos.call("World!")
     }
 }
---
+++ newClass.groovy
@@ -0,0 +1,9 @@
+public class newClass {
+    def myClosure = {
+       a,b,c->
+       y = a+b+c
+       println y
+}
+myClosure(1,2,3)
+
+}
